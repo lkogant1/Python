7e240ab41bf85f2eb3f02839f462a11c512fcdd6
lkogant1 <koganl001@gmail.com>
2018-05-02 07:17:37 +0530

    update

---
+++ basics1.py
@@ -0,0 +1,24 @@
+#Multiline statement
+# line continuation character \
+a = 1+2+2+4+ \
+	8+1
+print(a)
+
+#parentheses:tuple
+a = (1+4+5+6+7 
+	+5+0+
+	9+10)
+print(a)
+
+#square brackets: list
+st=['red','green',
+'blue',
+'violet']
+print(st)
+
+#curly braces: dictionary
+one={1:'one',2:'two',
+3:'three'}
+
+#multiple statements in a single line
+a=3;b=4;c=a+b;print(c)
---
+++ if_else.py
@@ -0,0 +1,11 @@
+#python if else loop
+#values of three variables equal or not
+#use logical operator "and"
+
+a = int(input("a: "))
+b = int(input("b: "))
+c = int(input("c: "))
+if(a==b and b==c):
+	print("All values are equal")
+else:
+	print("All values are not equal")
